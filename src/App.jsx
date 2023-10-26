a82a668b3ab981e062a11681b5bac14b8ab93d4c
Kanishk <kanishkteotia5077@gmail.com>
2023-10-26 16:01:06 +0530

    added style

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,37 @@
 import { useState } from 'react';
 import Board from './components/board';
+import StatusMessage from './components/statusMessage';
 import './styles.scss';
 import { calculateWinner } from './winner';
+
 function App() {
   const [square, setSquare] = useState(Array(9).fill(null));
   const [isNextX, setIsNextX] = useState(false);
-  const statusMsg = isNextX ? 'X' : 'O';
+
   const winner = calculateWinner(square);
-  const statusWin = winner
-    ? `Winner is ${winner}`
-    : `next player: ${statusMsg}`;
-  console.log('winner is ' + winner);
+
   const onClickSq = clickedPosition => {
-    if (square[clickedPosition]) {
+    if (square[clickedPosition] || winner) {
       return;
     }
-    if (winner) {
-      return;
-    }
+
     setSquare(currSq => {
       return currSq.map((val, position) => {
         if (clickedPosition === position) {
           return isNextX ? 'X' : 'O';
         }
+
         return val;
       });
     });
+
     setIsNextX(currIsNextX => !currIsNextX);
   };
 
   return (
     <div className="app">
+      <StatusMessage winner={winner} isNextX={isNextX} square={square} />
       <Board square={square} onClickSq={onClickSq} />
-      <h2>{statusWin}</h2>
     </div>
   );
 }
---
+++ src/components/statusMessage.jsx
@@ -0,0 +1,37 @@
+const StatusMessage = ({ winner, isNextX, square }) => {
+  const statusMsg = isNextX ? 'X' : 'O';
+  const noMoveLeft = square.every(val => val !== null);
+  const renderStatusMsg = () => {
+    if (winner) {
+      return (
+        <>
+          Winner :{' '}
+          <span className={winner === 'O' ? 'text-green' : 'text-orange'}>
+            {winner}
+          </span>
+        </>
+      );
+    }
+    if (!winner && noMoveLeft) {
+      return (
+        <>
+          <span className="text-orange">X</span> and{' '}
+          <span className="text-green">O</span> tied!
+        </>
+      );
+    }
+    if (!winner && !noMoveLeft) {
+      return (
+        <>
+          Next Player :{' '}
+          <span className={isNextX ? 'text-orange' : 'text-green'}>
+            {statusMsg}
+          </span>
+        </>
+      );
+    }
+    return null;
+  };
+  return <div className="status-message">{renderStatusMsg()}</div>;
+};
+export default StatusMessage;
